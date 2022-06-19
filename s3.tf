54c74d1d2c258972a5e3ab43ef7d749f030b2724
jerinrathnam <jerinrathnam.gem@gmail.com>
2022-06-19 20:33:44 +0530

    Made some changes

--- s3.tf
+++ s3.tf
@@ -51,7 +51,7 @@ resource "aws_s3_bucket" "web_bucket" {
   tags = local.common_tags
 }
 
-resource "aws_s3_bucket_object" "object1" {
+resource "aws_s3_bucket_object" "object" {
   for_each = {
     website = "/website/index.html"
     photo = "/website/Polish_20220517_223858461.png"
--- variables.tf
+++ variables.tf
@@ -1,25 +1,11 @@
-variable "aws_access_key" {
-  type      = string
-  sensitive = true
 
-}
 variable "naming_prefix" {
   type = string
   default = "jerin"
   
 }
 
-variable "aws_secret_key" {
-  type      = string
-  sensitive = true
 
-}
-
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-
-}
 variable "enable_dns_hostnames" {
   type    = bool
   default = true
