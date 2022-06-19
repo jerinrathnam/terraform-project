43619e4bed35303f852c0dc08532992d319de155
jerinrathnam <jerinrathnam.gem@gmail.com>
2022-06-19 20:41:18 +0530

    adding variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,25 @@
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
 
+}
 variable "naming_prefix" {
   type = string
   default = "jerin"
   
 }
 
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
 
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+
+}
 variable "enable_dns_hostnames" {
   type    = bool
   default = true
