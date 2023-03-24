d8f3061dc3970e5757c94775c32fdc0a9efce3b9
Jerin Rathnam <61724686+jerinrathnam@users.noreply.github.com>
2023-03-24 15:03:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,6 +15,9 @@ variable "naming_prefix" {
 }
 variable "instance_count" {
   type = map(number)
+  default = {
+    "Development" = 1
+  }
 }
 
 variable "aws_region" {
@@ -30,11 +33,15 @@ variable "enable_dns_hostnames" {
 
 variable "vpc_cidr_block" {
   type = map(string)
-
+  default = {
+    "Development" = "10.0.0.0/16
+    }
 }
 
 variable "vpc_subnet_count" {
   type = map(number)
+  default = {
+    "Development" = 2
 
 }
 
@@ -46,18 +53,29 @@ variable "map_public_ip_on_launch" {
 
 variable "instance_type" {
   type = map(string)
+  default = {
+    "Development" = "t2.micro"
+    }
 }
 
 variable "web_name" {
   type = string
+  default = {
+    "Development" = "myweb"
+    }
 }
 
 variable "name" {
   type = string
-
+  default = {
+    "Development" = "jerin"
+    }
 }
 
 variable "project" {
   type = string
+  default = {
+    "Development" = "terraform"
+    }
 }
 
